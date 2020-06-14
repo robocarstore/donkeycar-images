25d19f6ae6aa3229778feb57e7413960f503683a
Jonathan Tse <jonathan@tsez.net>
2020-06-14 11:49:17 +0800

    add low battery auto shutdown

---
+++ low-battery-auto-shutdown.py
@@ -0,0 +1,13 @@
+import board
+import busio
+import adafruit_ina219
+import subprocess
+
+i2c = busio.I2C(board.SCL, board.SDA)
+ina219 = adafruit_ina219.INA219(i2c, 0x41)
+
+current_voltage = ina219.bus_voltage
+
+if current_voltage < 7:
+    print("current voltage under 7. Shutting down to protect the pi")
+    subprocess.check_output(['sudo', 'shutdown', '-h', 'now'])
