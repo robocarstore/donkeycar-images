b4bbfda8d32094fdf8a1a20ebe413622dbe9bf43
Jonathan Tse <jonathan@tsez.net>
2020-06-14 11:52:29 +0800

    output current voltage to syslog

--- low-battery-auto-shutdown.py
+++ low-battery-auto-shutdown.py
@@ -7,6 +7,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 ina219 = adafruit_ina219.INA219(i2c, 0x41)
 
 current_voltage = ina219.bus_voltage
+print(f"Current voltage = {current_voltage}")
 
 if current_voltage < 7:
     print("current voltage under 7. Shutting down to protect the pi")
