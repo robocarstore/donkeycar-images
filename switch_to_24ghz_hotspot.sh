2c74c42f052f1d686caa351c4589a9e35cb189a3
Jonathan Tse <jonathan@tsez.net>
2021-05-20 20:36:49 +0800

    add info to switch band command

--- switch_to_24ghz_hotspot.sh
+++ switch_to_24ghz_hotspot.sh
@@ -4,6 +4,8 @@ SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 source $SRC_DIR/network-functions.sh
 
+echo "Please note that this will switch network band for this session. Hotspot band will fallback on next reboot. For permanent change please edit donkey.cfg"
+
 hotspot_down
 
 ln -sf /opt/donkeycar-images/resources/hostapd.24ghz.conf /etc/hostapd/hostapd.conf
